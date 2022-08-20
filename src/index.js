1abe2b692d73e15f3c78072247a1717a2fbd945d
Leonardo Navarro <abreunavarro.leonardo@gmail.com>
2022-08-19 21:52:26 -0300

    Aula de React - CSS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 'blueviolet', color: 'white', border: 'none',width: '100%', borderRadius: 8}
+const textoRotulo = 'Nome:'
+const textoBotao = () => 'Enviar'
+
+
 const App = () => {
   return (
-    <div>
+    <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+      <label classname="Label" htmfor="nome">{textoRotulo}</label>
+      <input style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: 768, borderRadius: 8, outline: 'none' }}/>
+      <button style={estilosBotao}>{textoBotao()}</button>
 
     </div>
   )
---
+++ src/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Label = styled.div`
+    display: block;
+    margin-bottom: 4px;
+`;
+ 
+export const Input = styled.div`
+    padding-top: 8px;
+    padding-bottom: 8px;
+    border-style: hidden;
+    width: 100%;
+    border-radius: 8px;
+    outline: none;
+`;
+
+export const Buttom = styled.div`
+    margin-top: 12px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    background-color: blueviolet;
+    color: white;
+    border: none;
+    border-radius: 8px;
+    width: 100%;
+`;
